0d4861dff12ca4e121b994bc14447daee395e76a
Garv nanwani <garvnanwani16@gmail.com>
2020-10-02 15:08:49 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config()
+
+const app = express();
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Database connected'));
+
+app.use(express.json());
+
+const subscribursRouter = require('./routes/subscribursRouter');
+
+app.listen(3000, () => {
+    console.log('Server started on PORT: 3000');
+})
